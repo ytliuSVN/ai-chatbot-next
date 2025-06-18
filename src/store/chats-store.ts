c84e5eaab591346edbc877e0e5f9bfe4727d5f6b
Penny <enderman542@gmail.com>
2025-06-18 14:02:50 +0800

    chore: devtools middleware zustand

--- src/store/chats-store.ts
+++ src/store/chats-store.ts
@@ -1,13 +1,19 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const chatsGlobalStore = create((set) => ({
-  // This will hold the current chat
-  selectedChat: null,
-  setSelectedChat: (data: unknown) => set({ selectedChat: data }),
+const chatsGlobalStore = create(
+  devtools(
+    (set) => ({
+      // This will hold the current chat
+      selectedChat: null,
+      setSelectedChat: (data: unknown) => set({ selectedChat: data }),
 
-  // This will hold the chats for the logged-in user
-  userChats: [],
-  setUserChats: (data: unknown) => set({ userChats: data }),
-}));
+      // This will hold the chats for the logged-in user
+      userChats: [],
+      setUserChats: (data: unknown) => set({ userChats: data }),
+    }),
+    // { enabled: false } // Disable devtools in production
+  )
+);
 
 export default chatsGlobalStore;
--- src/store/users-store.ts
+++ src/store/users-store.ts
@@ -1,10 +1,16 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const usersGlobalStore = create((set) => ({
-  loggedInUserData: null,
-  // Function to set the logged-in user data
-  // This function can be used to update the user data in the global state
-  setLoggedInUserData: (data: unknown) => set({ loggedInUserData: data }),
-}));
+const usersGlobalStore = create(
+  devtools(
+    (set) => ({
+      loggedInUserData: null,
+      // Function to set the logged-in user data
+      // This function can be used to update the user data in the global state
+      setLoggedInUserData: (data: unknown) => set({ loggedInUserData: data }),
+    }),
+    // { enabled: false }
+  )
+);
 
 export default usersGlobalStore;
